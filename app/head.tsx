9e5322f99bf63079b57089c824024f4686c5ba5b
Ross Ragsdale <ross@pointblankdev.com>
2023-04-18 16:11:12 -0500

    :mag: Update metadata

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,8 @@
-const title = 'React Three Next Starter'
-const url = 'https://react-three-next.vercel.app/'
-const description = 'The easiest and fastest way to create a 3D website using React Three Fiber and NextJS'
-const author = 'Author'
-const twitter = '@pmndrs'
+const title = 'Welshcorgicoin'
+const url = 'https://www.welshcorgicoin.org/'
+const description = 'The premier community token and symbol in the Stacks ecosystem.'
+const author = 'rozar.btc'
+const twitter = '@Communitywelsh'
 
 export default function Head() {
   return (
@@ -18,10 +18,7 @@ export default function Head() {
       {/* Search Engine Optimization Meta Tags */}
       <title>{title}</title>
       <meta name='description' content={description} />
-      <meta
-        name='keywords'
-        content='Software Engineer,Product Manager,Project Manager,Data Scientist,Computer Scientist'
-      />
+      <meta name='keywords' content='Bitcoin,Stacks,Meme,Crypto,Coin,Token,Welsh,Corgi,Doge,Shiba' />
       <meta name='robots' content='index,follow' />
       <meta name='distribution' content='web' />
       {/* 
