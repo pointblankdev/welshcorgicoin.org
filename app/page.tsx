28d54f82bea9276514340a7dc24c929655883c78
Ross Ragsdale <ross@pointblankdev.com>
2023-04-18 16:06:28 -0500

    :fire: Remove template code

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 
 import dynamic from 'next/dynamic'
 import { Suspense } from 'react'
+import LogoTitle from '../public/img/Welshicorgicoin+Logo-v2.png'
+import Image from 'next/image'
 
 const Logo: any = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Logo), { ssr: false })
 const Dog: any = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Dog), { ssr: false })
@@ -29,23 +31,20 @@ export default function Page() {
       <div className='mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5'>
         {/* jumbo */}
         <div className='flex w-full flex-col items-start justify-center p-12 text-center md:w-2/5 md:text-left'>
-          <p className='w-full uppercase'>Next + React Three Fiber</p>
-          <h1 className='my-4 text-5xl font-bold leading-tight'>Next 3D Starter</h1>
-          <p className='mb-8 text-2xl leading-normal'>A minimalist starter for React, React-three-fiber and Threejs.</p>
+          <Image className='my-4 text-5xl font-bold leading-tight' src={LogoTitle} alt='welshcorgicoin-logo' />
         </div>
 
-        <div className='w-full text-center md:w-3/5'>
+        {/* <div className='w-full text-center md:w-3/5'>
           <View className='flex h-96 w-full flex-col items-center justify-center'>
             <Suspense fallback={null}>
               <Logo route='/blob' scale={0.6} position={[0, 0, 0]} />
               <Common />
             </Suspense>
           </View>
-        </div>
+        </div> */}
       </div>
 
-      <div className='mx-auto flex w-full flex-col flex-wrap items-center p-12 md:flex-row  lg:w-4/5'>
-        {/* first row */}
+      {/* <div className='mx-auto flex w-full flex-col flex-wrap items-center p-12 md:flex-row  lg:w-4/5'>
         <div className='relative h-48 w-full py-6 sm:w-1/2 md:my-12 md:mb-40'>
           <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Events are propagated</h2>
           <p className='mb-8 text-gray-600'>Drag, scroll, pinch, and rotate the canvas to explore the 3D scene.</p>
@@ -58,7 +57,6 @@ export default function Page() {
             </Suspense>
           </View>
         </div>
-        {/* second row */}
         <div className='relative my-12 h-48 w-full py-6 sm:w-1/2 md:mb-40'>
           <View orbit className='relative h-full animate-bounce sm:h-48 sm:w-full'>
             <Suspense fallback={null}>
@@ -76,7 +74,7 @@ export default function Page() {
             scroll along, resize, etc.
           </p>
         </div>
-      </div>
+      </div> */}
     </>
   )
 }
