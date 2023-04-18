8288cd2523b4ac9a82520406234fdc1fe90bd7e2
Ross Ragsdale <ross@pointblankdev.com>
2023-04-18 18:25:12 -0500

    :dizzy: Add 3d animation logo

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,9 @@
 
 import dynamic from 'next/dynamic'
 import { Suspense } from 'react'
-import LogoTitle from '../public/img/Welshicorgicoin+Logo-v2.png'
-import Image from 'next/image'
 
-const Logo: any = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Logo), { ssr: false })
-const Dog: any = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Dog), { ssr: false })
-const Duck: any = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Duck), { ssr: false })
-const View: any = dynamic(() => import('@/components/canvas/View').then((mod) => mod.View), {
+const Logo = dynamic(() => import('@/components/canvas/Welsh').then((mod) => mod.Logo), { ssr: false })
+const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.View), {
   ssr: false,
   loading: () => (
     <div className='flex h-96 w-full flex-col items-center justify-center'>
@@ -23,58 +19,28 @@ const View: any = dynamic(() => import('@/components/canvas/View').then((mod) =>
     </div>
   ),
 })
-const Common: any = dynamic(() => import('@/components/canvas/View').then((mod) => mod.Common), { ssr: false })
+const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mod.Common), { ssr: false })
 
 export default function Page() {
   return (
     <>
-      <div className='mx-auto flex w-full flex-col flex-wrap items-center md:flex-row  lg:w-4/5'>
+      <div className='relative mx-auto flex w-full flex-col flex-wrap items-center md:flex-row'>
         {/* jumbo */}
-        <div className='flex w-full flex-col items-start justify-center p-12 text-center md:w-2/5 md:text-left'>
-          <Image className='my-4 text-5xl font-bold leading-tight' src={LogoTitle} alt='welshcorgicoin-logo' />
+        <div className='absolute z-10 flex w-full flex-col items-start justify-center p-4 text-center text-black md:w-2/5 md:text-left'>
+          <p className='w-full uppercase'>Community Run</p>
+          <h1 className='my-4 text-5xl font-bold leading-tight'>Welshcorgicoin</h1>
+          <p className='mb-8 text-2xl leading-normal'>The cutest token in the Stacks ecosystem.</p>
         </div>
 
-        {/* <div className='w-full text-center md:w-3/5'>
-          <View className='flex h-96 w-full flex-col items-center justify-center'>
+        <div className='w-full text-center'>
+          <View className='flex h-screen w-full flex-col items-center justify-center'>
             <Suspense fallback={null}>
-              <Logo route='/blob' scale={0.6} position={[0, 0, 0]} />
-              <Common />
+              <Logo scale={2} position={[0, -1.6, 0]} rotation={[0.0, -0.3, 0]} />
+              <Common color={'#f6f6f6'} />
             </Suspense>
           </View>
-        </div> */}
+        </div>
       </div>
-
-      {/* <div className='mx-auto flex w-full flex-col flex-wrap items-center p-12 md:flex-row  lg:w-4/5'>
-        <div className='relative h-48 w-full py-6 sm:w-1/2 md:my-12 md:mb-40'>
-          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Events are propagated</h2>
-          <p className='mb-8 text-gray-600'>Drag, scroll, pinch, and rotate the canvas to explore the 3D scene.</p>
-        </div>
-        <div className='relative my-12 h-48 w-full py-6 sm:w-1/2 md:mb-40'>
-          <View orbit className='relative h-full  sm:h-48 sm:w-full'>
-            <Suspense fallback={null}>
-              <Dog scale={2} position={[0, -1.6, 0]} rotation={[0.0, -0.3, 0]} />
-              <Common color={'lightpink'} />
-            </Suspense>
-          </View>
-        </div>
-        <div className='relative my-12 h-48 w-full py-6 sm:w-1/2 md:mb-40'>
-          <View orbit className='relative h-full animate-bounce sm:h-48 sm:w-full'>
-            <Suspense fallback={null}>
-              <Duck route='/blob' scale={2} position={[0, -1.6, 0]} />
-              <Common color={'lightblue'} />
-            </Suspense>
-          </View>
-        </div>
-        <div className='w-full p-6 sm:w-1/2'>
-          <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Dom and 3D are synchronized</h2>
-          <p className='mb-8 text-gray-600'>
-            3D Divs are renderer through the View component. It uses gl.scissor to cut the viewport into segments. You
-            tie a view to a tracking div which then controls the position and bounds of the viewport. This allows you to
-            have multiple views with a single, performant canvas. These views will follow their tracking elements,
-            scroll along, resize, etc.
-          </p>
-        </div>
-      </div> */}
     </>
   )
 }
---
+++ src/components/canvas/Welsh.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import * as THREE from 'three'
+import { useGLTF } from '@react-three/drei'
+import { useFrame, useThree } from '@react-three/fiber'
+import { useEffect, useRef } from 'react'
+
+export const Logo = ({ route = '/blob', ...props }) => {
+  const welsh = useGLTF('/welsh3.glb')
+  const { camera } = useThree()
+  const initialPosition = useRef(new THREE.Vector3())
+  const time = useRef(0)
+  const spinTime = useRef(0)
+  const spinAxis = useRef(new THREE.Vector3(0, 1, 0))
+  const spinDuration = useRef(0)
+  const nextSpinTimeout = useRef(0) as any
+
+  const easeOutCubic = (t) => {
+    t = t - 1
+    return t * t * t + 1
+  }
+
+  const scheduleNextSpin = () => {
+    nextSpinTimeout.current = setTimeout(() => {
+      const randomAxis = Math.random() > 0.5 ? 0 : 1
+      spinAxis.current.set(randomAxis === 0 ? 1 : 0, randomAxis === 1 ? 1 : 0, 0)
+      spinDuration.current = Math.random() * 0.5 + 0.5
+      spinTime.current = 0
+      scheduleNextSpin()
+    }, (Math.random() * 15 + 5) * 1000)
+  }
+
+  useEffect(() => {
+    // Set the Welsh scene position and scale
+    welsh.scene.position.copy(initialPosition.current)
+    welsh.scene.scale.multiplyScalar(1)
+
+    scheduleNextSpin()
+
+    return () => {
+      clearTimeout(nextSpinTimeout.current)
+    }
+  }, [welsh.scene])
+
+  useFrame((state, delta) => {
+    // Face the Welsh scene toward the camera
+    welsh.scene.lookAt(camera.position)
+
+    // Update time
+    time.current += delta
+
+    // Update the Welsh scene position using sinusoidal motion
+    welsh.scene.position.set(
+      initialPosition.current.x + Math.sin(time.current) * 0.05,
+      initialPosition.current.y + Math.sin(time.current * 1.5) * 0.05 - 0.3,
+      initialPosition.current.z + Math.sin(time.current * 2) * 0.05,
+    )
+
+    // Update the Welsh scene rotation for a decelerating spin
+    if (spinTime.current < spinDuration.current) {
+      spinTime.current += delta
+      const spinProgress = spinTime.current / spinDuration.current
+      const easedProgress = easeOutCubic(spinProgress)
+      welsh.scene.rotateOnAxis(spinAxis.current, easedProgress * Math.PI * 2)
+    }
+  })
+
+  return (
+    <>
+      <primitive object={welsh.scene} {...props} />
+    </>
+  )
+}
